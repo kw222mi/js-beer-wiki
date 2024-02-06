f0ace291337849b1be750ae718ba5e2a508dfcc2
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-02-06 13:38:14 +0100

    add details

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const getRandomBeer = async (e) => {
     const response = await fetch("https://api.punkapi.com/v2/beers/random");
     const randomBeer = await response.json()
     console.log(randomBeer[0])
-    renderRandomBeer(randomBeer[0].name, randomBeer[0].image_url)
+    renderRandomBeer(randomBeer)
 }
 
-const renderRandomBeer = (name, url) => {
+const renderRandomBeer = (randomBeer) => {
      const contentDiv = document.getElementById("content");
     // Clear old beers
     while (contentDiv.hasChildNodes()) {
@@ -24,18 +24,32 @@ const renderRandomBeer = (name, url) => {
     }
     // Render new beer
     const beerHTML = `
-    <img src=${url} alt="picture of random beer" class="random-beer-img">
-    <div>${name}</div>
+    <img src=${randomBeer[0].image_url} alt="picture of random beer" class="random-beer-img">
+    <div>${randomBeer[0].name}</div>
     <button id="seeMoreBtn">See more</button>
     `;
     contentDiv.innerHTML = beerHTML
 
     const seeMoreBtn = document.getElementById("seeMoreBtn");
     seeMoreBtn.addEventListener("click", () => {
-      displayBeerDetailsPage(beer);
+      displayBeerDetailsPage(randomBeer);
     });
 }
 
-const displayBeerDetailsPage = () => {
-
+function displayBeerDetailsPage(beer) {
+    beer = beer[0]
+  const beerDetailsHTML = `
+    <div class="beer-details">
+      <h2>${beer.name}</h2>
+      <img src="${beer.image_url}" alt="${beer.name}">
+      <p>Description: ${beer.description}</p>
+      <p> Alcohol by volume/ABV: ${beer.abv}</p>
+      <p>Volume: ${beer.volume.value} ${beer.volume.unit}</p>
+      <p>Ingredients: ${JSON.stringify(beer.ingredients)}</p>
+      <p>Hops: ${JSON.stringify(beer.ingredients.hops)}</p>
+      <p>Food Pairing: ${beer.food_pairing.join(", ")}</p>
+      <p>Brewers Tips: ${beer.brewers_tips}</p>
+    </div>
+  `;
+  content.innerHTML = beerDetailsHTML;
 }
