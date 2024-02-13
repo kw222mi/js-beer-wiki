9573fa56473589444dcc3e4d2349088938f0283c
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-02-13 13:26:39 +0100

    refactor and clean up the code

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ beerButton.addEventListener("click", (e) => getRandomBeer(e));
 // Event listener to search for a beer
 searchForBeerButton.addEventListener("click", () => getSearch());
 // Event listener to get a random beer when loading the page
-// window.addEventListener("load", (e) => getRandomBeer(e), checkState());
+window.addEventListener("load", (e) => renderDefaultContent());
 
 // Event listener for popstate event
 window.addEventListener("popstate", (event) => {
   event.preventDefault();
-  console.log(history);
-  console.log("EVENT STATE " + event.state.view);
   if (event.state.view) {
     // Restore application state based on the popped history state
     restoreAppState(event.state);
@@ -31,23 +29,9 @@ const getRandomBeer = async (e) => {
   try {
     const response = await fetch("https://api.punkapi.com/v2/beers/random");
     const randomBeer = await response.json();
-    // console.log(randomBeer[0]);
     renderRandomBeer(randomBeer);
-
     // Push the state to history
-    const state = {
-      view: "randomBeer", // Anger vilken vy användaren befinner sig i
-      searchData: {
-        searchTerm: "", // Sparar söktermen
-        searchResults: [], // Sparar sökresultaten
-      },
-      randomBeerData: {
-        randomBeer: randomBeer, // Sparar den slumpmässiga ölen
-      },
-    };
-
-    history.pushState(state, "", "");
-    console.log(history.state);
+    pushRandomBeerState(randomBeer);
   } catch (error) {
     console.error(error);
   }
@@ -65,7 +49,7 @@ const renderRandomBeer = (randomBeer) => {
      </div>
     `;
   contentDiv.innerHTML = beerHTML;
-
+  // Add event listener
   const seeMoreBtn = document.getElementById("seeMoreBtn");
   seeMoreBtn.addEventListener("click", () => {
     displayBeerDetailsPage(randomBeer[0]);
@@ -73,9 +57,8 @@ const renderRandomBeer = (randomBeer) => {
 };
 
 // Function to display detailed information about a beer
-function displayBeerDetailsPage(beer) {
+const  displayBeerDetailsPage = (beer) => {
   clearOldContent();
-  //beer = beer[0];
 
   // Create a list of hops
   const hopsList = beer.ingredients.hops
@@ -103,200 +86,193 @@ function displayBeerDetailsPage(beer) {
     </div>
   `;
   content.innerHTML = beerDetailsHTML;
+  pushBeerDetailsState(beer);
+  }
 
-  const state = {
-    view: "beerDetails", // Anger vilken vy användaren befinner sig i
-    searchData: {
-      searchTerm: "", // Sparar söktermen
-      searchResults: [], // Sparar sökresultaten
-    },
-    randomBeerData: {
-      randomBeer: beer, // Sparar den slumpmässiga ölen
-    },
-  };
-  history.pushState(state, "", "");
-}
-
-// Function to handle the search for a beer
-const getSearch = () => {
-  clearOldContent();
-
-  // Render new content
-  const searchFormHTML = `
+  // Function to handle the search for a beer
+  const getSearch = () => {
+    clearOldContent();
+    // Render the searchform
+    const searchFormHTML = `
     <form id="searchForm">
       <input type="text" id="searchInput" placeholder="Search beer...">
       <button type="submit">Search</button>
     </form>
     <div id="searchResults"></div>
   `;
-  contentDiv.innerHTML = searchFormHTML;
+    contentDiv.innerHTML = searchFormHTML;
 
-  // Add event listener to the search form
-  const searchForm = document.getElementById("searchForm");
-  searchForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const searchTerm = document.getElementById("searchInput").value;
-    const modifiedSearchTerm = searchTerm.replace(/\s+/g, "_"); // Replace spaces with underscore
-    const searchResults = await searchBeerByName(modifiedSearchTerm);
-    displaySearchResults(searchResults);
-
-    const state = {
-      view: "searchResults", // Anger vilken vy användaren befinner sig i
-      searchData: {
-        searchTerm: modifiedSearchTerm, // Sparar söktermen
-        searchResults: searchResults, // Sparar sökresultaten
-      },
-      randomBeerData: {
-        randomBeer: null, // Sparar den slumpmässiga ölen
-      },
-    };
-
-    history.pushState(state, "");
-    console.log(history);
-  });
-};
-
-// Function to search for a beer by its name
-const searchBeerByName = async (searchTerm) => {
-  try {
-    const response = await fetch(
-      `https://api.punkapi.com/v2/beers?beer_name=${searchTerm}`
-    );
-    const searchBeer = await response.json();
-    // console.log(searchBeer[0]);
-    return searchBeer;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// Function to display search results with pagination
-const displaySearchResults = (results) => {
-  const searchResultsDiv = document.getElementById("searchResults");
-  const totalResults = results.length;
-  const itemsPerPage = 10;
-  let currentPage = 1;
-
-  // Function to render search results with pagination
-  const renderResults = () => {
-    const start = (currentPage - 1) * itemsPerPage;
-    const end = start + itemsPerPage;
-    const slicedResults = results.slice(start, end);
-
-    let html = "<ul>";
-    slicedResults.forEach((beer) => {
-      html += `<li><a href="#" class="beer-link" data-id="${beer.id}">${beer.name}</a></li>`;
+    // Add event listener to the search form
+    const searchForm = document.getElementById("searchForm");
+    searchForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const searchTerm = document.getElementById("searchInput").value;
+      const modifiedSearchTerm = searchTerm.replace(/\s+/g, "_"); // Replace spaces with underscore
+      const searchResults = await searchBeerByName(modifiedSearchTerm);
+      displaySearchResults(searchResults);
+      pushSearchState(modifiedSearchTerm, searchResults);
     });
-    html += "</ul>";
+  };
 
-    // Add pagination controls
-    const paginationHTML = `
+  // Function to search for a beer by its name
+  const searchBeerByName = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `https://api.punkapi.com/v2/beers?beer_name=${searchTerm}`
+      );
+      const searchBeer = await response.json();
+      return searchBeer;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Function to display search results with pagination
+  const displaySearchResults = (results) => {
+      const searchResultsDiv = document.getElementById("searchResults");
+      const totalResults = results.length;
+      const itemsPerPage = 10;
+      let currentPage = 1;
+      const start = (currentPage - 1) * itemsPerPage;
+      const end = start + itemsPerPage;
+      const slicedResults = results.slice(start, end);
+      // Create a list with seachresults
+      let html = "<ul>";
+      slicedResults.forEach((beer) => {
+        html += `<li><a href="#" class="beer-link" data-id="${beer.id}">${beer.name}</a></li>`;
+      });
+      html += "</ul>";
+
+      // Add pagination controls
+      const paginationHTML = `
       <div class="pagination">
         <button class="prev-btn" ${
           currentPage === 1 ? "disabled" : ""
         }>Prev</button>
         <span class="page-info">${start + 1}-${Math.min(
-      end,
-      totalResults
-    )} / ${totalResults}</span>
+        end,
+        totalResults
+      )} / ${totalResults}</span>
         <button class="next-btn" ${
           end >= totalResults ? "disabled" : ""
         }>Next</button>
       </div>
     `;
+      searchResultsDiv.innerHTML = html + paginationHTML;
 
-    searchResultsDiv.innerHTML = html + paginationHTML;
-
-    // Add event listeners for beer links
-    const beerLinks = document.querySelectorAll(".beer-link");
-    beerLinks.forEach((link) => {
-      link.addEventListener("click", async (event) => {
-        event.preventDefault();
-        const beerId = event.target.dataset.id;
-        const beer = await getBeerById(beerId);
-        // console.log(beer[0]);
-
-        displayBeerDetailsPage(beer[0]);
+      // Add event listeners for beer links
+      const beerLinks = document.querySelectorAll(".beer-link");
+      beerLinks.forEach((link) => {
+        link.addEventListener("click", async (event) => {
+          event.preventDefault();
+          const beerId = event.target.dataset.id;
+          const beer = await getBeerById(beerId);
+          displayBeerDetailsPage(beer[0]);
+        });
       });
-    });
 
-    // Add event listeners for pagination buttons
-    const prevBtn = document.querySelector(".prev-btn");
-    const nextBtn = document.querySelector(".next-btn");
+      // Add event listeners for pagination buttons
+      const prevBtn = document.querySelector(".prev-btn");
+      const nextBtn = document.querySelector(".next-btn");
 
-    prevBtn.addEventListener("click", () => {
-      if (currentPage > 1) {
-        currentPage--;
-        renderResults();
-      }
-    });
+      prevBtn.addEventListener("click", () => {
+        if (currentPage > 1) {
+          currentPage--;
+          renderResults();
+        }
+      });
 
-    nextBtn.addEventListener("click", () => {
-      if (end < totalResults) {
-        currentPage++;
-        renderResults();
-      }
-    });
+      nextBtn.addEventListener("click", () => {
+        if (end < totalResults) {
+          currentPage++;
+          renderResults();
+        }
+      });
+    };
+
+  // Function to fetch a beer by its ID
+  const getBeerById = async (id) => {
+    try {
+      const response = await fetch(`https://api.punkapi.com/v2/beers/${id}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Function to clear old content from the contentDiv
+  const clearOldContent = () => {
+    while (contentDiv.hasChildNodes()) {
+      contentDiv.removeChild(contentDiv.firstChild);
+    }
   };
 
-  renderResults();
-};
+  // Function to restore application state based on history state
+  const restoreAppState = (state) => {
+    switch (state.view) {
+      case "randomBeer":
+        renderRandomBeer(state.randomBeerData.randomBeer);
+        break;
+      case "beerDetails":
+        displayBeerDetailsPage(state.randomBeerData.randomBeer);
+        break;
+      case "searchResults":
+        getSearch();
+        displaySearchResults(state.searchData.searchResults);
+        break;
+      default:
+        console.log("render default");
+        renderDefaultContent();
+        break;
+    }
+  };
 
-// Function to fetch a beer by its ID
-async function getBeerById(id) {
-  try {
-    // console.log("ID " + id);
-    const response = await fetch(`https://api.punkapi.com/v2/beers/${id}`);
-    const data = await response.json();
+  // Function to render default content
+  const renderDefaultContent = () => {
+    console.log("default");
+    getRandomBeer();
+  };
 
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
+  // Push state to history
+  const pushRandomBeerState = (randomBeer) => {
+    const state = {
+      view: "randomBeer",
+      searchData: {
+        searchTerm: "",
+        searchResults: [],
+      },
+      randomBeerData: {
+        randomBeer: randomBeer,
+      },
+    };
+    history.pushState(state, "", "");
+  };
+
+  const pushBeerDetailsState = (beer) => {
+  const state = {
+    view: "beerDetails",
+    searchData: {
+      searchTerm: "",
+      searchResults: [],
+    },
+    randomBeerData: {
+      randomBeer: beer,
+    },
+  };
+  history.pushState(state, "", "");
 }
 
-// Function to clear old content from the contentDiv
-const clearOldContent = () => {
-  while (contentDiv.hasChildNodes()) {
-    contentDiv.removeChild(contentDiv.firstChild);
-  }
-};
-
-// Function to restore application state based on history state
-const restoreAppState = (state) => {
-  console.log(location);
-  console.log(history);
-  console.log(state.view);
-  // Restore application state based on the popped history state
-  switch (state.view) {
-    case "randomBeer":
-      console.log("get random");
-      console.log(state.randomBeerData.randomBeer);
-      renderRandomBeer(state.randomBeerData.randomBeer);
-      break;
-    case "beerDetails":
-      console.log("beerDetails");
-      console.log(state.randomBeerData.randomBeer);
-      displayBeerDetailsPage(state.randomBeerData.randomBeer);
-      break;
-    case "searchResults":
-      // Restore search results
-      console.log("search results");
-      console.log(state.searchData.searchResults);
-      getSearch()
-      displaySearchResults(state.searchData.searchResults);
-      break;
-    default:
-      console.log("render default");
-      renderDefaultContent();
-      break;
-  }
-};
-
-// Function to render default content
-const renderDefaultContent = () => {
-  // Render default content
-  // For example, render a random beer as default content
-  console.log("default");
-  getRandomBeer();
-};
+const pushSearchState = (modifiedSearchTerm, searchResults) => {
+      const state = {
+        view: "searchResults",
+        searchData: {
+          searchTerm: modifiedSearchTerm,
+          searchResults: searchResults,
+        },
+        randomBeerData: {
+          randomBeer: null,
+        },
+      };
+      history.pushState(state, "", "");
+    }
